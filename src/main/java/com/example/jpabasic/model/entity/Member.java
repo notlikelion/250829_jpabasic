6674ed415a2937f83a7c9dce17bded15511485dc
notlikelion <notlikelion@gmail.com>
2025-08-29 11:23:19 +0900

    lombok 우당탕탕...

--- src/main/java/com/example/jpabasic/model/entity/Member.java
+++ src/main/java/com/example/jpabasic/model/entity/Member.java
@@ -4,8 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity // 감지해서 테이블이 있는지 체크하고 객체와 연결
+//@Getter // .get~
+//@Setter // .set~
+//@ToString // sout -> 구성요소
+//@NoArgsConstructor // 기본 생성자 (패러미터가 없는...)
+//@RequiredArgsConstructor // final로 되어 있는 것들을 만들어주는 생성자 entity 용 아님
+//@AllArgsConstructor // 모든 프로퍼티
+@Data // @Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode
+@NoArgsConstructor // new Member()
+@AllArgsConstructor // new Member(... - 필드들을 직접 만들어서 넣을 때)
+// 생성자는 뭔가 직접 정의를 하면 기본 생성자는 비활성화된다 -> 직접 추가해줘야함
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) // 헷갈리면 알아서 해주세요...
@@ -14,4 +25,6 @@ public class Member {
 //    @GeneratedValue(strategy = GenerationType.UUID) - 오늘 최종 실습
     private Long id;
     private String name;
+
+    // 생성으로도 만들어줄 수 있는데...
 }
--- src/main/java/com/example/jpabasic/service/MemberService.java
+++ src/main/java/com/example/jpabasic/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.jpabasic.service;
 
+import com.example.jpabasic.model.entity.Member;
 import com.example.jpabasic.model.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +12,10 @@ public class MemberService {
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
         System.out.println("Service <- Repository 주입 완료!");
+        // Getter, Setter가 필요하다
+        Member newMember = new Member();
+//        newMember.setId(); // 필요가 없다 -> 왜? -> GenerateValue? 알아서 실행된다
+        newMember.setName("김자바");
+        this.memberRepository.save(newMember);
     }
 }
